63c8242db1c5fdb015e352d2a9bba25080233658
siniourz <kazemisina45@gmail.com>
2025-03-06 12:29:35 +0300

    changing back the name of the static folder and then change the setting.py

--- exstore/settings.py
+++ exstore/settings.py
@@ -117,9 +117,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]  # Ensure this exists
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Needed for collectstatic
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'users', 'static'),  # Add this line
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Needed for collectstatic
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
